c1ac962e8fd2fc0833c310a06bab4325e3723f89
nabil <nabil.andriantomanga@gmail.com>
2019-09-21 20:52:50 +0200

    first commit

---
+++ src/main/java/com/andriantomanga/webfluxdataelasticpoc/WebfluxDataElasticPocApplication.java
@@ -0,0 +1,13 @@
+package com.andriantomanga.webfluxdataelasticpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDataElasticPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxDataElasticPocApplication.class, args);
+	}
+
+}
